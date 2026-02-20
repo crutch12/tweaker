0c51c44bdb9fc4b7f4c2cd25f706717d9f072305
Konstantin Barabanov <criitch@yandex.ru>
2026-02-20 12:55:23 +0300

    chore: migrate to postcss-media-to-container

--- packages/devtools-ui/postcss.config.mjs
+++ packages/devtools-ui/postcss.config.mjs
@@ -5,52 +5,9 @@ import postcssCustomMedia from "postcss-custom-media";
 import postcssCombineDuplicatedSelectors from "postcss-combine-duplicated-selectors";
 import postcssDiscardEmpty from "postcss-discard-empty";
 import postcssWhitespace from "@radix-ui/themes/postcss-whitespace.cjs";
+import postcssMediaToContainer from "postcss-media-to-container";
 import autoprefixer from "autoprefixer";
 
-/**  @type import('postcss').PluginCreator<{ containerName?: string, filter?: (path: string) => boolean }> */
-const postcssReplaceMediaWithContainer = ({
-  containerName = "",
-  filter = () => true,
-}) => {
-  const pluginName = "replace-media-with-container";
-
-  const supportedRules = [
-    "width",
-    "min-width",
-    "max-width",
-    "height",
-    "min-height",
-    "max-height",
-    "inline-size",
-    "min-inline-size",
-    "block-size",
-    "min-block-size",
-    "aspect-ratio",
-    "orientation",
-  ];
-
-  return {
-    postcssPlugin: pluginName,
-    OnceExit(root) {
-      const filePath = root.source?.input.file || "<unknown>";
-
-      if (!filter(filePath)) return;
-
-      console.log(pluginName, "processing file", filePath);
-
-      root.walkAtRules("media", (atRule) => {
-        const ruleIsSupported = supportedRules.some((rule) =>
-          atRule.params.includes(`${rule}:`),
-        );
-        if (!ruleIsSupported) return;
-        atRule.name = "container";
-        atRule.params = [containerName.trim() + " ", atRule.params]
-          .filter(Boolean)
-          .join("");
-      });
-    },
-  };
-};
 /**
  * Since we import @radix-ui/themes/src/styles/index.css to override breakpoints, we should implement the same postcss config
  * @see https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/postcss.config.cjs
@@ -64,7 +21,7 @@ export default {
     postcssCombineDuplicatedSelectors(),
     postcssDiscardEmpty(),
     postcssWhitespace(),
-    postcssReplaceMediaWithContainer({
+    postcssMediaToContainer({
       containerName: "tweaker-devtools",
       filter: (path) => Boolean(path.match(/styles[\\/]radix\.css/)),
     }),
