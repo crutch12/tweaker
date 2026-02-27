d2922f128af7cc9ed3c7933dc84a19cbc3596895
Konstantin Barabanov <criitch@yandex.ru>
2026-02-27 15:22:25 +0300

    chore: disable includePrototypes

--- packages/devtools-ui/src/features/messages/MessageRow.tsx
+++ packages/devtools-ui/src/features/messages/MessageRow.tsx
@@ -105,7 +105,7 @@ export function MessageRow({
           <ObjectInspector
             sortKeys={false}
             expandLevel={0}
-            includePrototypes={true}
+            includePrototypes={false}
             data={originalValueData}
           />
         </Table.Cell>
